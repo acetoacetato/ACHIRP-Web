6d2951e09a514844bada17d31ef8b843b6cc1723
Alen <alen.figueroa.m@mail.pucv.cl>
2020-12-12 22:27:50 -0300

    puerto

--- server.js
+++ server.js
@@ -105,8 +105,7 @@ const httpsServer = https.createServer(/*credentials,*/ app);
 
 //Se abre el servidor en el puerto que salga o en el 9000 si es devStart
 
-app.listen(process.env.PORT || 80)
-
+app.listen(process.env.PORT || 8080)
 
 
 httpServer.listen(80, () => {
